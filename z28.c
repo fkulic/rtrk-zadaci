948e6bcaa7bae1978b45caba91ee6708fc3df9a7
rtrk <filip.kulic@outlook.com>
2017-10-30 11:46:53 +0100

    reading matrix from file

--- z28.c
+++ z28.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define rows 8
-#define columns 10
+#define ROWS 8
+#define COLUMNS 10
  
 struct Node {
 	int data;
@@ -11,14 +12,18 @@ struct Node {
 };
 
 void enQueue(struct Node** front, int newData);
-int deQueue(struct Node* node);
+int deQueue(struct Node** front);
 void printList(struct Node* node);
-void readImageIntoMatrix(int matrix[][columns], int row, int column);
+void readImageIntoMatrix(int matrix[][COLUMNS]);
+void printMatrix(int matrix[][COLUMNS]);
 
 int main()
 {
 	struct Node* q = NULL;
-	int img[rows][columns];
+	int img[ROWS][COLUMNS];
+	
+	
+	
 	enQueue(&q, 1);
 	enQueue(&q, 2);
 	enQueue(&q, 3);
@@ -27,12 +32,14 @@ int main()
 	enQueue(&q, 6);
 	printList(q);
 	
-	deQueue(q);
-	deQueue(q);
-	deQueue(q);
+	deQueue(&q);
+	deQueue(&q);
+	deQueue(&q);
 	printList(q);
+	
 
-	readImageIntoMatrix(img, rows, columns);
+	readImageIntoMatrix(img);
+	printMatrix(img);
 	
     return 0;
 }
@@ -53,20 +60,27 @@ void enQueue(struct Node** front, int newData) {
 	}
 }
 
-int deQueue(struct Node* node) {
-	if (node != NULL) {
+int deQueue(struct Node** front) {
+	if (*front != NULL) {
 		int value;
-		struct Node* newLast;
+		
+		if((*front)->next == NULL) {
+			printf("One element.");
+			value = (*front)->data;
+			free(*front);
+			*front = NULL;
+			return value;
+		}
+		
+		struct Node* node = *front;
 		
 		while(node->next != NULL) {
 			node = node->next;
 		}
 		
 		value = node->data;
-		newLast = node->prev;
-		
+		node->prev->next = NULL;		
 		free(node);
-		newLast->next = NULL;
 		
 		return value;
 	}
@@ -80,20 +94,36 @@ void printList(struct Node *node) {
 	}
 	printf("\n");
 }
-
-void readImageIntoMatrix(int matrix[][columns], int row, int column) {
+void readImageIntoMatrix(int matrix[][COLUMNS]) {
 	// use fgets() to get one line at a time
 	// use sscanf() with strtok to extract integers
 	
 	FILE* img;
 	char line[200];
 	img = fopen("image.txt", "r");
+	char *token;
 	
 	if (img) {
-		while (fgets(line, 200, img)) {
-			puts(line);
+		for (int i = 0; fgets(line, 200, img); i++) {
+			token = strtok(line, " ");
+			for (int j = 0; token != NULL; j++) {
+				sscanf(token, "%d", &matrix[i][j]);
+				printf("%d ", matrix[i][j]);
+				token = strtok(NULL, " ");
+			}
+			printf("\n");
+			
 		}
 		fclose(img);
 	}
 }
+
+void printMatrix(int matrix[][COLUMNS]) {
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; i < COLUMNS; j++) {
+			printf("%d ", matrix[i][j]);
+		}
+		printf("\n");
+	}
+} 
 
